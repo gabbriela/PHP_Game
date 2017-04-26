5d0cfe989b87afeebfa57aa70ece2ac62482a298
Gabriela <gabi.ivanova@gmail.com>
2017-04-26 10:33:40 +0300

    basic controllers

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -2,28 +2,15 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Map;
-use AppBundle\Entity\UserUnits;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/*
+ * @Route('/map');
+ */
 class MapController extends Controller
 {
-    /**
-     * @Route("/map", name="map")
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function mapAction()
+    public function indexAction($name)
     {
-        $currentUser = $this->getUser();
-
-        $map = $this->getDoctrine()->getRepository(Map::class)->findBy(array(), array('positionY' => 'ASC', 'positionX' => 'ASC'));
-
-        $size = count($map);
-
-        $user = $this->getUser();
-
-        return $this->render("information/map.html.twig",
-            ['mapAll' => $map, 'currentUser' => $currentUser, 'size'=>$size]);
+        return $this->render('', array('name' => $name));
     }
 }
